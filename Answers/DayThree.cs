51fbc29ed26ba1087eeacaee866806fdf856b4ee
Nathalie Everts <n.everts@gmail.com>
2021-12-03 19:29:59 +0100

    day three

---
+++ Answers/DayThree.cs
@@ -0,0 +1,43 @@
+﻿using Answers.Logic;
+using Answers.Utilities;
+
+namespace Answers
+{
+    internal class DayThreePartOne : IAnswer
+    {
+        public string Get()
+        {
+            var input = InputReader.ReadFile("day3part1.txt");
+            return PowerConsumptionLogic.GetPowerConsumption(input);
+        }
+
+        public int GetMenuOrder()
+        {
+            return 5;
+        }
+
+        public string GetMenuTitle()
+        {
+            return "Day Three Part One";
+        }
+    }
+
+    internal class DayThreePartTwo : IAnswer
+    {
+        public string Get()
+        {
+            var input = InputReader.ReadFile("day3part1.txt");
+            return LifeSupportLogic.GetRating(input);
+        }
+
+        public int GetMenuOrder()
+        {
+            return 6;
+        }
+
+        public string GetMenuTitle()
+        {
+            return "Day Three Part Two";
+        }
+    }
+}
---
+++ Tests/PowerConsumptionLogicTests.cs
@@ -0,0 +1,31 @@
+﻿using Answers.Logic;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace Tests
+{
+    public class PowerConsumptionLogicTests
+    {
+        private readonly List<string> _input = new()
+        {
+            "00100",
+            "11110",
+            "10110",
+            "10111",
+            "10101",
+            "01111",
+            "00111",
+            "11100",
+            "10000",
+            "11001",
+            "00010",
+            "01010"
+        };
+
+        [Test]
+        public void GetPowerConsumption()
+        {
+            Assert.AreEqual("198", PowerConsumptionLogic.GetPowerConsumption(_input));
+        }
+    }
+}
