bc10a76e5359811fc36cbbb26a8f066db55ef99c
Nathalie Everts <n.everts@gmail.com>
2021-12-07 18:43:49 +0100

    day seven part two

---
+++ Answers/DaySeven.cs
@@ -0,0 +1,44 @@
+﻿using Answers.Logic;
+using Answers.Utilities;
+
+namespace Answers
+{
+    internal class DaySevenPartOne : IAnswer
+    {
+        public string Get()
+        {
+            var input = InputReader.ReadFile("day7part1.txt");
+            return CrabSubmarineLogic.GetFuelConsumption(input);
+        }
+
+        public int GetMenuOrder()
+        {
+            return 13;
+        }
+
+        public string GetMenuTitle()
+        {
+            return "Day Seven Part One";
+        }
+    }
+
+    internal class DaySevenPartTwo : IAnswer
+    {
+        public string Get()
+        {
+            var input = InputReader.ReadFile("day7part1.txt");
+            //var input = new List<string>() { "16,1,2,0,4,2,7,1,2,14" };
+            return CrabSubmarineLogic.GetCumulativeFuelConsumption(input);
+        }
+
+        public int GetMenuOrder()
+        {
+            return 14;
+        }
+
+        public string GetMenuTitle()
+        {
+            return "Day Seven Part Two";
+        }
+    }
+}
---
+++ Answers/Logic/CrabSubmarineLogic.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Answers.Logic
+{
+    public class CrabSubmarineLogic
+    {
+        public static string GetFuelConsumption(List<string> input)
+        {
+            var crabs = input[0].Split(',').Select(f => int.Parse(f)).ToList();
+            var lowestFuelConsumption = long.MaxValue;
+
+            for (int i = 0; i < crabs.Count; i++)
+            {
+                var fuelConsumption = 0L;
+                for (int j = 0; j < crabs.Count; j++)
+                {
+                    fuelConsumption += Math.Abs(crabs[i]-crabs[j]);
+                }
+                if (fuelConsumption < lowestFuelConsumption)
+                    lowestFuelConsumption = fuelConsumption;
+            }
+
+            return lowestFuelConsumption.ToString();
+        }
+
+        public static string GetCumulativeFuelConsumption(List<string> input)
+        {
+            var crabs = input[0].Split(',').Select(f => int.Parse(f)).ToList();
+            var lowestPosition = crabs.Min();
+            var highestPosition = crabs.Max();
+            var lowestFuelConsumption = long.MaxValue;
+
+            for (int i = lowestPosition; i <= highestPosition; i++)
+            {
+                var fuelConsumption = 0L;
+                for (int j = 0; j < crabs.Count; j++)
+                {
+                    var distance = Math.Abs(crabs[j] - i);
+                    fuelConsumption += distance * (distance + 1) / 2;
+                }
+                if (fuelConsumption < lowestFuelConsumption)
+                    lowestFuelConsumption = fuelConsumption;
+            }
+
+            return lowestFuelConsumption.ToString();
+        }
+    }
+}
